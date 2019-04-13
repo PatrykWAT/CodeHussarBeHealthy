dd53fe0376c921ddee082583d2bad472d44f9eff
Sepci0 <ppolgon@gmail.com>
2019-04-13 22:18:01 +0200

    branch sidebar archive, new navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,13 @@
 import React, { Component } from 'react';
 
+import MyNavbar from './components/Mynvabar'
+
 class App extends Component {
   render() {
     return (
       <>
+        <MyNavbar />
+
       </>
     );
   }
---
+++ frontend/src/components/Mynvabar.js
@@ -0,0 +1,104 @@
+import React from 'react';
+import styled from 'styled-components'
+import { Link } from 'react-scroll'
+
+
+
+import {
+    Collapse,
+    Navbar,
+    NavbarToggler,
+    NavbarBrand,
+    Nav,
+    NavItem,
+    // NavLink,
+    UncontrolledDropdown,
+    DropdownToggle,
+    DropdownMenu,
+    DropdownItem
+} from 'reactstrap';
+
+
+const Logo = styled.img`
+
+height: 50px;
+width: auto;
+`
+
+const MyLink = styled(Link)`
+
+:hover{
+    cursor:pointer;
+}
+
+`
+
+class MyNavbar extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.toggle = this.toggle.bind(this);
+        this.state = {
+            isOpen: false
+        };
+    }
+    toggle() {
+        this.setState({
+            isOpen: !this.state.isOpen
+        });
+    }
+    render() {
+        return (
+            <div>
+                <Navbar color="light" className="bg-white" light expand="md">
+                    <NavbarBrand href="/">
+                        <Logo alt="logo" className="logo" src="/assets/logo.png"></Logo>
+                    </NavbarBrand>
+                    <NavbarToggler onClick={this.toggle} />
+                    <Collapse isOpen={this.state.isOpen} navbar>
+                        <Nav className="ml-auto mr-auto" navbar>
+                            <NavItem>
+                                <MyLink className="nav-link" activeClass="active" to="live" spy={true} smooth={true} duration={500} >
+                                    Live Stats</MyLink>
+                            </NavItem>
+                            <NavItem>
+                                <MyLink className="nav-link" activeClass="active" to="summary" spy={true} smooth={true} duration={500} >
+                                    Summary</MyLink>
+                            </NavItem>
+                            {/* <NavItem>
+
+                                <MyLink className="nav-link" activeClass="active" to="Stats" spy={true} smooth={true} duration={500} >
+                                    Stats</MyLink>
+
+                            </NavItem> */}
+
+
+                            <UncontrolledDropdown nav inNavbar>
+                                <DropdownToggle nav caret>
+                                    Stats
+                                </DropdownToggle>
+                                <DropdownMenu right>
+                                    <DropdownItem>
+                                        <MyLink className="nav-link" activeClass="active" to="Stats" spy={true} smooth={true} duration={500} >
+                                            Firs Stats</MyLink>
+                                    </DropdownItem>
+                                    <DropdownItem>
+                                        <MyLink className="nav-link" activeClass="active" to="Stats" spy={true} smooth={true} duration={500} >
+                                            Second Stat</MyLink>
+                                    </DropdownItem>
+                                    {/* <DropdownItem divider /> */}
+                                    <DropdownItem>
+                                        <MyLink className="nav-link" activeClass="active" to="Stats" spy={true} smooth={true} duration={500} >
+                                            Third Stat</MyLink>
+                                    </DropdownItem>
+                                </DropdownMenu>
+                            </UncontrolledDropdown>
+                        </Nav>
+                    </Collapse>
+                </Navbar>
+            </div>
+        );
+    }
+}
+
+export default MyNavbar 
