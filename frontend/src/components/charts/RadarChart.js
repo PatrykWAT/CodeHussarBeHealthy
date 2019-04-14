0c5c737d455d92fe63d18098c7adc3171bbcb314
Sepci0 <ppolgon@gmail.com>
2019-04-14 04:11:47 +0200

    tensor flow initial commit

--- frontend/src/components/charts/RadarChart.js
+++ frontend/src/components/charts/RadarChart.js
@@ -25,12 +25,17 @@ export default class BigChart extends PureComponent {
     // static jsfiddleUrl = 'https://jsfiddle.net/alidingling/6ebcxbx4/';
 
 
+    labelsCustom = (e) => {
+        console.log(e)
+        return e
+    }
+
     render() {
         return (
             <ResponsiveContainer height={800} width="100%">
                 <RadarChart outerRadius="80%" data={data}  >
                     <PolarGrid />
-                    <PolarAngleAxis orientation="outer" dataKey="subject"  tickFormatter={this.labelsCustom} />
+                    <PolarAngleAxis orientation="outer" dataKey="subject" tickFormatter={this.labelsCustom} />
                     {/* <PolarRadiusAxis angle={90} orientation="right"  /> */}
                     {/* <Label value="Pages of my website" offset={0} position="insideBottom" /> */}
                     <Radar dataKey="A" stroke="#FF00c7" fill="#FF00c7" fillOpacity={0.6} />
